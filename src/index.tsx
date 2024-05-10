799a7571e70bf06b95a91128037bdb06a0ea1b2a
Zachary H <zth12301@gmail.com>
2024-05-09 19:59:21 -0700

    router and pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
+import Resume from './pages/resume';
+import Contact from './pages/contact';
+
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/resume",
+    element: <Resume />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ src/pages/contact.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Contact() {
+    return (
+        <div>
+            contact
+        </div>
+    );
+}
