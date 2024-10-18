cf3d15e661899fcc2b7047b1e6a928dbb4879988
Zachary H <zth12301@gmail.com>
2024-10-17 20:59:19 -0700

    background + bio + flexbox

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,25 +4,26 @@ import Navbar from '../components/Navbar';
 export default function Home() {
     return (
         <>
-                {/* <Navbar />  */}
         <div className="page">
+
+            <div className='top-row-wrap'>
+                <div className='bio-box'>
+                    <p className='bio-text'>
+                        Zachary Hanczyc. UW CS Class of 2027
+                    </p>
+                </div>
+
+                <div className="face-wrapper">
+                    <img src={placeholderImage} className="face"/>
+
+                </div>
+            </div>
+
         </div>
 
-        <div className="face-wrapper">
-                <img src={placeholderImage} className="face"/>
-            </div>
-            <div className="face-wrapper">
-                <img src={placeholderImage} className="face"/>
-            </div>
-            <div className="face-wrapper">
-                <img src={placeholderImage} className="face"/>
-            </div>
-            <div className="face-wrapper">
-                <img src={placeholderImage} className="face"/>
-            </div>
-            <div className="face-wrapper">
-                <img src={placeholderImage} className="face"/>
-            </div>
+        <div className='background'>
+        </div>
+
         </>
     );
 }
